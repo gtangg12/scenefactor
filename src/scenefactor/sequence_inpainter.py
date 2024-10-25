fae50ec6e5b3e4a049aa52d3cf7737f0e00358af
George Tang <gtangg12@gmail.com>
2024-10-24 22:12:37 -0400

    Update

--- src/scenefactor/sequence_inpainter.py
+++ src/scenefactor/sequence_inpainter.py
@@ -11,6 +11,35 @@ from scenefactor.utils.visualize import *
 from scenefactor.factorization_common import *
 
 
+def compute_inpaint_radius(
+    bmask: NumpyTensor['h', 'w'], 
+    imask: NumpyTensor['h', 'w'], 
+    ratio: float, clip_min=15, clip_max=75, bound_iterations=20, bound_threshold=5
+) -> int:
+    """
+    """
+    # assume bmask is a circle: r = sqrt(VOL / pi)(sqrt(c) - 1)
+    assert ratio > 1
+    radius = int(np.sqrt(np.sum(bmask) / np.pi * (ratio - 1)))
+    radius = int(np.clip(radius, clip_min, clip_max))
+
+    # ensure dialation doesn't overpaint non adjacent regions
+    num_labels = len(np.unique(imask[dialate_bmask(bmask, clip_min)]))
+    lo = 0
+    hi = radius
+    for _ in range(bound_iterations):
+        mid = (lo + hi) // 2
+        num_labels_current = len(np.unique(imask[dialate_bmask(bmask, mid)]))
+        if num_labels_current > num_labels:
+            hi = mid
+        else:
+            lo = mid
+        if hi - lo < bound_threshold:
+            break
+    radius = min(radius, lo)
+    return radius
+
+
 class SequenceInpainter:
     """
     """
