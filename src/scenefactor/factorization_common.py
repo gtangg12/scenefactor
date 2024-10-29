959fcff352a7ac3208438bc1c02e3cc18609c6bf
George Tang <gtangg12@gmail.com>
2024-10-29 16:02:16 -0400

    Remove unneccessary import

--- src/scenefactor/factorization_common.py
+++ src/scenefactor/factorization_common.py
@@ -2,7 +2,6 @@ import numpy as np
 
 from scenefactor.data.common import NumpyTensor
 from scenefactor.data.sequence import FrameSequence
-from scenefactor.utils.geom import dialate_bmask
 
 
 SEMANTIC_BACKGROUND = 0
